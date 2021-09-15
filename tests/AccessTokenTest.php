3ded707e05678ac330be1554b577958d3d021c68
stefanius <sgrootveld@testmonitor.com>
2021-09-15 17:05:20 +0200

    Increase Test Coverage

--- tests/AccessTokenTest.php
+++ tests/AccessTokenTest.php
@@ -32,4 +32,30 @@ class AccessTokenTest extends TestCase
         // Then
         $this->assertEquals('', $channel);
     }
+
+    /** @test */
+    public function it_should_return_an_empty_array_when_the_team_is_not_provided()
+    {
+        // Given
+        $token = new AccessToken('12345', '123456', time() + 3600, ['incoming_webhook' => ['redirect' => 'https:/redirect.com']]);
+
+        // When
+        $team = $token->team();
+
+        // Then
+        $this->assertEquals([], $team);
+    }
+
+    /** @test */
+    public function it_should_return_the_id_of_the_team()
+    {
+        // Given
+        $token = new AccessToken('12345', '123456', time() + 3600, ['incoming_webhook' => ['channel' => '#testing'], 'team' => ['id' => 1]]);
+
+        // When
+        $team = $token->team();
+
+        // Then
+        $this->assertEquals(['id' => 1], $team);
+    }
 }
