170f7b231407205a5df32f5e9bcfc3206782b768
stefanius <sgrootveld@testmonitor.com>
2021-09-20 15:28:02 +0200

    Increase Test Coverage

--- tests/SlackResourceOwnerTest.php
+++ tests/SlackResourceOwnerTest.php
@@ -69,4 +69,201 @@ class SlackResourceOwnerTest extends TestCase
         // Then
         $this->assertEquals('#AABBCC', $response);
     }
+
+    /** @test */
+    public function it_should_return_the_profile_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+            ]);
+
+        // When
+        $response = $resourceOwner->getProfile();
+
+        // Then
+        $this->assertIsArray($response);
+        $this->assertEquals($profile, $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_first_name_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getFirstName();
+
+        // Then
+        $this->assertEquals('Joanne', $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_last_name_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getLastName();
+
+        // Then
+        $this->assertEquals('Doe', $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_skype_username_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getSkype();
+
+        // Then
+        $this->assertEquals('jdoe@testmonitor.com', $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_email_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getEmail();
+
+        // Then
+        $this->assertEquals('jdoe@testmonitor.com', $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_phonenumber_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => '',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getPhone();
+
+        // Then
+        $this->assertEquals('06123456789', $response);
+    }
+
+    /** @test */
+    public function it_should_return_the_avatar_from_the_resource_owner()
+    {
+        // Given
+        $profile = [
+            'first_name' => 'Joanne',
+            'last_name' => 'Doe',
+            'real_name' => 'The Real Joanne Doe',
+            'email' => 'jdoe@testmonitor.com',
+            'skype' => 'jdoe@testmonitor.com',
+            'phone' => '06123456789',
+            'image_24' => 'avatar24',
+            'image_32' => '',
+            'image_48' => '',
+            'image_72' => '',
+            'image_192' => '',
+
+        ];
+        $resourceOwner = new SlackResourceOwner(['user' => ['profile' => $profile]
+        ]);
+
+        // When
+        $response = $resourceOwner->getImage24();
+
+        // Then
+        $this->assertEquals('avatar24', $response);
+    }
 }
