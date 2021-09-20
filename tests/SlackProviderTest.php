71f239f6ad7669088000309e7555c602b207fe6e
stefanius <sgrootveld@testmonitor.com>
2021-09-20 15:42:18 +0200

    Increase Test Coverage

--- tests/SlackProviderTest.php
+++ tests/SlackProviderTest.php
@@ -22,7 +22,7 @@ class SlackProviderTest extends TestCase
     }
 
     /** @test */
-    public function it_should_not_instantiate_the_slack_provider_when_required_options_are_missing()
+    public function it_can_not_instantiate_the_slack_provider_when_required_options_are_missing()
     {
         $this->expectException(\InvalidArgumentException::class);
 
@@ -30,7 +30,7 @@ class SlackProviderTest extends TestCase
     }
 
     /** @test */
-    public function it_should_retrieve_the_autorized_user_test_url()
+    public function it_can_retrieve_the_autorized_user_test_url()
     {
         // When
         $provider = new SlackProvider([
@@ -64,7 +64,7 @@ class SlackProviderTest extends TestCase
     }
 
     /** @test */
-    public function it_should_retrieve_the_base_access_token_url()
+    public function it_can_retrieve_the_base_access_token_url()
     {
         // When
         $provider = new SlackProvider([
